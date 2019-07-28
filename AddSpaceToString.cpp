02766c0a177a07d6e0ab22eb311540dec7289b39
xu dong <yanxuceo@gmail.com>
2019-07-28 11:11:44 +0200

    an example for passing-value-by-reference

---
+++ AddSpaceToString.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+
+// has input parameters of constant referenced pointer to
+// char data type 
+// Note: Notice that function doesn't have any return values.
+
+/**
+ * An example passing-value-by-reference
+ *
+ * @param str1 a reference to a pointer pointing to const char
+ * 
+ */
+void addSpaces(const char * &str1) { 
+    char *temp = new char(sizeof(str1)*2);
+    char *start = temp;
+    // moving our pointer to the end of array of character
+    while(*str1 != '\0'){
+        *temp++ = *str1++;
+        *temp++ = ' ';
+    }
+  
+    str1 = start;
+}
+
+int main() {
+    std::string str = "Hello World";
+    const char* ptr = str.c_str();
+    std::cout << "string is: "<< ptr << std::endl;
+
+    addSpaces(ptr);
+    std::cout<<"string is: "<<ptr<<std::endl;
+    
+    return 0;
+}
